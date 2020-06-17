75752729d2a7e43956be736216bfee1fcd2ffbe3
Md Nayeem <codernayeem898@gmail.com>
2020-06-17 18:19:33 +0600

    Changed to a full new layout

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,231 +1,171 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import (QApplication, QLabel, QGridLayout, QLineEdit, QSizePolicy, QStyleFactory,  QWidget, QToolButton, QShortcut, QPushButton)
-from PyQt5.QtCore import Qt, QRect
-from PyQt5.QtGui import QKeySequence, QFont
+from PyQt5.QtWidgets import QApplication, QStyleFactory, QMainWindow
+from PyQt5.QtGui import QTextCursor
 import sys
+from ui_set import set_ui, set_key_sequence
 
 
-class Calculator(QWidget):
-    all_key_that_can_entered = '1234567890-+*x/.()'
-    errors = ['ZeroDivisionError', 'SyntaxError', 'Error']
+appctxt = ApplicationContext()
+
+
+class Calculator(QMainWindow):
+    errors = ['ZeroDivisionError', 'SyntaxError', 'Error', 'MathError', 'NotANumberError', 'PasteError']
+    operators = ['+', '-', '*', '/']
+    last_invalid_exp = None
+    memory = 0.0
 
     def __init__(self, parent=None):
         super(Calculator, self).__init__(parent)
-        self.setWindowTitle("Calculator")
         QApplication.setStyle(QStyleFactory.create('Fusion'))
-        QApplication.setPalette(QApplication.style().standardPalette())
+        set_ui(self, globals()['appctxt'].get_resource('backspace.png'), globals()['appctxt'].get_resource('icon.png'))
+        set_key_sequence(self)
 
-        font = self.font()
-        font.setPointSize(font.pointSize() + 12)
-
-        QShortcut(QKeySequence('Return'), self).activated.connect(lambda: self.if_clicked('='))
-        QShortcut(QKeySequence('='), self).activated.connect(lambda: self.if_clicked('='))
-        QShortcut(QKeySequence('1'), self).activated.connect(lambda: self.if_clicked('1'))
-        QShortcut(QKeySequence('2'), self).activated.connect(lambda: self.if_clicked('2'))
-        QShortcut(QKeySequence('3'), self).activated.connect(lambda: self.if_clicked('3'))
-        QShortcut(QKeySequence('4'), self).activated.connect(lambda: self.if_clicked('4'))
-        QShortcut(QKeySequence('5'), self).activated.connect(lambda: self.if_clicked('5'))
-        QShortcut(QKeySequence('6'), self).activated.connect(lambda: self.if_clicked('6'))
-        QShortcut(QKeySequence('7'), self).activated.connect(lambda: self.if_clicked('7'))
-        QShortcut(QKeySequence('8'), self).activated.connect(lambda: self.if_clicked('8'))
-        QShortcut(QKeySequence('9'), self).activated.connect(lambda: self.if_clicked('9'))
-        QShortcut(QKeySequence('0'), self).activated.connect(lambda: self.if_clicked('0'))
-        QShortcut(QKeySequence('+'), self).activated.connect(lambda: self.if_clicked('+'))
-        QShortcut(QKeySequence('-'), self).activated.connect(lambda: self.if_clicked('-'))
-        QShortcut(QKeySequence('*'), self).activated.connect(lambda: self.if_clicked('*'))
-        QShortcut(QKeySequence('/'), self).activated.connect(lambda: self.if_clicked('/'))
-        QShortcut(QKeySequence('('), self).activated.connect(lambda: self.if_clicked('('))
-        QShortcut(QKeySequence(')'), self).activated.connect(lambda: self.if_clicked(')'))
-        QShortcut(QKeySequence('.'), self).activated.connect(lambda: self.if_clicked('.'))
-        QShortcut(QKeySequence('Backspace'), self).activated.connect(lambda: self.if_clicked('Back'))
-        QShortcut(QKeySequence('c'), self).activated.connect(lambda: self.if_clicked('C'))
-
-        self.set_ui()
-
-    def set_ui(self):
-        self.setStyleSheet('QPushButton {background: #e6ffff} QPushButton:pressed {background: #e6ffe6} QLabel {background: #e6ffff; padding: 5px;}')
-
-        self.expression_entry = QLabel(self)
-        self.expression_entry.setGeometry(QRect(0, 0, 241, 61))
-        font = QFont()
-        font.setFamily("Arial")
-        font.setPointSize(18)
-        self.expression_entry.setFont(font)
-        self.expression_entry.setAlignment(Qt.AlignRight | Qt.AlignTrailing | Qt.AlignVCenter)
-        font = QFont()
-        font.setPointSize(12)
-
-        self.bt_c = QPushButton(self)
-        self.bt_c.setGeometry(QRect(0, 60, 61, 61))
-        self.bt_c.setFont(font)
-        self.bt_c.setStyleSheet('QPushButton {background: red}')
-
-        self.bt_back = QPushButton(self)
-        self.bt_back.setGeometry(QRect(60, 60, 121, 61))
-        self.bt_back.setFont(font)
-        self.bt_back.setStyleSheet('QPushButton {background: #ffffe6} QPushButton:pressed {background: #e6ffe6}')
-
-        self.bt_plus = QPushButton(self)
-        self.bt_plus.setGeometry(QRect(180, 60, 61, 61))
-        self.bt_plus.setFont(font)
-        self.bt_plus.setStyleSheet('QPushButton {background: #ffd11a} QPushButton:pressed {background: #ffcc00}')
-
-        self.bt_3 = QPushButton(self)
-        self.bt_3.setGeometry(QRect(120, 119, 61, 61))
-        self.bt_3.setFont(font)
-
-        self.bt_1 = QPushButton(self)
-        self.bt_1.setGeometry(QRect(0, 119, 61, 61))
-        self.bt_1.setFont(font)
-
-        self.bt_2 = QPushButton(self)
-        self.bt_2.setGeometry(QRect(60, 119, 61, 61))
-        self.bt_2.setFont(font)
-
-        self.bt_minus = QPushButton(self)
-        self.bt_minus.setGeometry(QRect(180, 119, 61, 61))
-        self.bt_minus.setFont(font)
-        self.bt_minus.setStyleSheet('QPushButton {background: #ffd11a} QPushButton:pressed {background: #ffcc00}')
-
-        self.bt_6 = QPushButton(self)
-        self.bt_6.setGeometry(QRect(120, 179, 61, 61))
-        self.bt_6.setFont(font)
-
-        self.bt_4 = QPushButton(self)
-        self.bt_4.setGeometry(QRect(0, 179, 61, 61))
-        self.bt_4.setFont(font)
-
-        self.bt_5 = QPushButton(self)
-        self.bt_5.setGeometry(QRect(60, 179, 61, 61))
-        self.bt_5.setFont(font)
-
-        self.bt_multiply = QPushButton(self)
-        self.bt_multiply.setGeometry(QRect(180, 179, 61, 61))
-        self.bt_multiply.setFont(font)
-        self.bt_multiply.setStyleSheet('QPushButton {background: #ffd11a} QPushButton:pressed {background: #ffcc00}')
-
-        self.bt_9 = QPushButton(self)
-        self.bt_9.setGeometry(QRect(120, 239, 61, 61))
-        self.bt_9.setFont(font)
-
-        self.bt_7 = QPushButton(self)
-        self.bt_7.setGeometry(QRect(0, 239, 61, 61))
-        self.bt_7.setFont(font)
-
-        self.bt_8 = QPushButton(self)
-        self.bt_8.setGeometry(QRect(60, 239, 61, 61))
-        self.bt_8.setFont(font)
-
-        self.bt_divide = QPushButton(self)
-        self.bt_divide.setGeometry(QRect(180, 239, 61, 61))
-        self.bt_divide.setFont(font)
-        self.bt_divide.setStyleSheet('QPushButton {background: #ffd11a} QPushButton:pressed {background: #ffcc00}')
-
-        self.bt_equal = QPushButton(self)
-        self.bt_equal.setGeometry(QRect(180, 299, 61, 61))
-        self.bt_equal.setFont(font)
-        self.bt_equal.setStyleSheet('QPushButton {background: #ffd11a} QPushButton:pressed {background: #ffcc00}')
-
-        self.bt_0 = QPushButton(self)
-        self.bt_0.setGeometry(QRect(0, 299, 121, 61))
-        self.bt_0.setFont(font)
-
-        self.bt_dot = QPushButton(self)
-        self.bt_dot.setGeometry(QRect(120, 299, 61, 61))
-        self.bt_dot.setFont(font)
-        self.bt_dot.setStyleSheet('QPushButton {background: #ffffe6} QPushButton:pressed {background: #ccffcc}')
-
-        self.expression_entry.setText("0")
-        self.bt_c.setText("C")
-        self.bt_back.setText("Backspace")
-        self.bt_plus.setText("+")
-        self.bt_3.setText("3")
-        self.bt_1.setText("1")
-        self.bt_2.setText("2")
-        self.bt_minus.setText("-")
-        self.bt_6.setText("6")
-        self.bt_4.setText("4")
-        self.bt_5.setText("5")
-        self.bt_multiply.setText("x")
-        self.bt_9.setText("9")
-        self.bt_7.setText("7")
-        self.bt_8.setText("8")
-        self.bt_divide.setText("/")
-        self.bt_equal.setText("=")
-        self.bt_0.setText("0")
-        self.bt_dot.setText(".")
-
-        self.set_onclick_event()
-
-    def set_onclick_event(self):
-        self.bt_c.clicked.connect(lambda: self.if_clicked("C"))
-        self.bt_back.clicked.connect(lambda: self.if_clicked("Back"))
-        self.bt_plus.clicked.connect(lambda: self.if_clicked("+"))
-        self.bt_3.clicked.connect(lambda: self.if_clicked("3"))
-        self.bt_1.clicked.connect(lambda: self.if_clicked("1"))
-        self.bt_2.clicked.connect(lambda: self.if_clicked("2"))
-        self.bt_minus.clicked.connect(lambda: self.if_clicked("-"))
-        self.bt_6.clicked.connect(lambda: self.if_clicked("6"))
-        self.bt_4.clicked.connect(lambda: self.if_clicked("4"))
-        self.bt_5.clicked.connect(lambda: self.if_clicked("5"))
-        self.bt_multiply.clicked.connect(lambda: self.if_clicked("*"))
-        self.bt_9.clicked.connect(lambda: self.if_clicked("9"))
-        self.bt_7.clicked.connect(lambda: self.if_clicked("7"))
-        self.bt_8.clicked.connect(lambda: self.if_clicked("8"))
-        self.bt_divide.clicked.connect(lambda: self.if_clicked("/"))
-        self.bt_equal.clicked.connect(lambda: self.if_clicked("="))
-        self.bt_0.clicked.connect(lambda: self.if_clicked("0"))
-        self.bt_dot.clicked.connect(lambda: self.if_clicked("."))
-
-    def if_clicked(self, bt=None):
-        old = str(self.expression_entry.text()).replace('x', '*')
-        if bt:
-            if bt == "C":
-                self.set_text_on_board('0')
-            elif bt == "Back":
-                if old[:len(old)-1] == '':
-                    self.set_text_on_board('0')
-                elif old != '0':
-                    self.set_text_on_board(old[:len(old)-1])
-
-            if bt == "=":
-                if old != '':
-                    self.show_result(old)
+    def if_clicked(self, bt):
+        if bt in '1234567890.()+-*/':
+            exp = self.get_exp()
+            if bt in Calculator.operators:
+                if exp not in Calculator.errors:
+                    self.set_exp(exp + str(bt))
+            elif exp not in Calculator.errors:
+                if exp == '0' and bt != '.':
+                    self.set_exp(str(bt))
+                else:
+                    self.set_exp(exp + str(bt))
             else:
-                if bt != 'C':
-                    for a_char in old:
-                        if a_char not in Calculator.all_key_that_can_entered:
-                            self.set_text_on_board('0')
-                if bt not in ['C', 'Back']:
-                    if old == '0' and bt != '.':
-                        self.set_text_on_board(str(bt))
+                if bt == '.':
+                    bt = '0.'
+                self.set_exp(str(bt))
+
+    def on_plus_minus_click(self):
+        exp = self.get_exp()
+        if exp not in Calculator.errors:
+            if exp.startswith('-'):
+                self.set_exp(exp[1:])
+            else:
+                self.set_exp('-' + exp)
+
+    def on_square_or_cube(self, s):
+        if str(s) in '23456789':
+            exp = self.get_exp()
+            if exp not in Calculator.errors:
+                res = self.get_result(f'({exp})**{s}', double_star_allow=True)
+                if res:
+                    if res in Calculator.errors:
+                        self.last_invalid_exp = exp
+                    self.set_exp(str(res))
+
+    def on_root_click(self, root):
+        if str(root) in '23456789':
+            exp = self.get_exp()
+            if exp not in Calculator.errors:
+                res = self.get_result(exp)
+                if res:
+                    if res in Calculator.errors:
+                        self.last_invalid_exp = exp
+                        self.set_exp(res)
                     else:
-                        self.set_text_on_board(old + str(bt))
+                        if root in range(0, 10, 2) and res.startswith('-'):
+                            self.last_invalid_exp = exp
+                            self.set_exp(Calculator.errors[3])
+                        else:
+                            res = self.get_result(f'{res}**(1/{root})', double_star_allow=True)
+                            if res:
+                                if res in Calculator.errors:
+                                    self.last_invalid_exp = exp
+                                    self.set_exp(exp)
+                                else:
+                                    if str(res).endswith('.0'):
+                                        res = str(res).replace('.0', '')
+                                    self.set_exp(res)
 
-    def set_text_on_board(self, text):
-        self.expression_entry.setText(str(text).replace('*', 'x'))
+    def on_back_click(self):
+        exp = self.get_exp()
+        if exp not in Calculator.errors and exp != '0':
+            if exp[:len(exp) - 1] != '':
+                self.set_exp(exp[:len(exp) - 1])
+            else:
+                self.set_exp('0')
 
-    def show_result(self, exp):
-        ex = str(exp)
-        if ex == '()':
-            ans = Calculator.errors[1]
-        elif ex in Calculator.errors:
-            ans = '0'
-        else:
-            try:
-                ans = eval(ex)
-            except ZeroDivisionError:
-                ans = Calculator.errors[0]
-            except SyntaxError:
-                ans = Calculator.errors[1]
-            except:
-                ans = Calculator.errors[2]
-        self.set_text_on_board(str(ans))
+    def on_ac_click(self):
+        self.last_invalid_exp = None
+        self.memory = 0.0
+        self.set_exp('0')
+
+    def on_c_click(self):
+        self.set_exp('0')
+
+    # 0 = get current memory or 1 = plus memory or 2 = minus memory or 3 = clear memory
+    def on_all_m_click(self, do=0):
+        exp = self.get_exp()
+        if do == 3:
+            self.memory = 0.0
+        elif exp not in Calculator.errors:
+            if do == 0:
+                r = str(self.memory)
+                if r.endswith('.0'):
+                    r = r.replace('.0', '')
+                if exp == '0':
+                    self.set_exp(r)
+                else:
+                    self.set_exp(exp + r)
+            elif do in (1, 2):
+                res = self.get_result(exp)
+                if res:
+                    if res in Calculator.errors:
+                        self.last_invalid_exp = exp
+                        self.set_exp(res)
+                    try:
+                        exp_float = float(res)
+                        if do == 1:
+                            self.memory += exp_float
+                        elif do == 2:
+                            self.memory -= exp_float
+                    except:
+                        self.last_invalid_exp = exp
+                        self.set_exp(Calculator.errors[4])
+
+    def on_ce_click(self):
+        exp = self.get_exp()
+        if exp in Calculator.errors and self.last_invalid_exp:
+            self.set_exp(self.last_invalid_exp)
+
+    def on_equal_click(self):
+        exp = self.get_exp()
+        res = self.get_result(exp)
+        if res:
+            if res in Calculator.errors:
+                self.last_invalid_exp = exp
+            self.set_exp(res)
+
+    def get_result(self, exp, double_star_allow=False):
+        if exp not in Calculator.errors:
+            if exp == '()' or (not double_star_allow and '**' in exp):
+                return Calculator.errors[1]
+            else:
+                try:
+                    ans = eval(exp)
+                    if str(ans).endswith('.0'):
+                        ans = str(ans).replace('.0', '')
+                    return str(ans)
+                except ZeroDivisionError:
+                    return Calculator.errors[0]
+                except SyntaxError:
+                    return Calculator.errors[1]
+                except:
+                    return Calculator.errors[2]
+        return None
+
+    def set_exp(self, text):
+        text = str(text).replace('*', '\u00d7').replace('/', '\u00f7')
+        self.exp_label.setPlainText(text)
+        self.exp_label.moveCursor(QTextCursor.End)
+
+    def get_exp(self):
+        return str(self.exp_label.toPlainText()).replace('\u00d7', '*').replace('\u00f7', '/').replace('\n', '')
 
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()
     window = Calculator()
-    window.resize(241, 361)
+    window.setWindowTitle("Calculator")
     window.show()
     sys.exit(appctxt.app.exec_())
